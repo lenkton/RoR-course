172cf513e2f1e0d697bf30bdef5b96bcbbdfc1e9
lenkton <antonov_s@yahoo.com>
2020-11-07 20:03:51 +0300

    big methods redesigned

--- railroad/text_ui.rb
+++ railroad/text_ui.rb
@@ -11,78 +11,58 @@ class TextUI
     @routes = {}
   end
 
-  def create_object(args)
-    if args[0] == '--help'
-      puts CREATE_HELP
+  def create_station(args)
+    return puts CREATE_FORMAT_ERROR if args.empty?
+
+    name = args[0]
+
+    return puts "Station #{name} already exists!" if @stations.include?(name)
+
+    @stations[name] = Station.new(name)
+    puts "Station #{name} created successfully"
+  end
+
+  def create_route(args)
+    return unless correct_form?(args, %i[any station station])
+
+    name, first, last = args
+
+    if @routes.include?(name)
+      puts "The Route #{name} already exists!"
       return
     end
 
-    if args.size < 2
+    @routes[name] = Route.new(@stations[first], @stations[last], name)
+    puts "The Route #{name} from #{first} to #{last} created successfully"
+  end
+
+  def create_train(args)
+    return puts CREATE_FORMAT_ERROR if args.size != 2
+
+    name, type = args
+
+    return puts "The Train #{name} already exists!" if @trains.include?(name)
+
+    case type
+    when 'cargo', 'car'
+      @trains[name] = CargoTrain.new(name)
+      puts "Cargo Train #{name} created successfully"
+    when 'passenger', 'p'
+      @trains[name] = PassengerTrain.new(name)
+      puts "Passenger Train #{name} created successfully"
+    else
       puts CREATE_FORMAT_ERROR
-      return
     end
+  end
+
+  def create_object(args)
+    return puts CREATE_FORMAT_ERROR if args.empty?
 
     case args[0]
-    # station creation
-    when 'station'
-      if @stations.include?(args[1])
-        puts 'The Station already exists!'
-        return
-      end
-
-      @stations[args[1]] = Station.new(args[1])
-      puts "Station #{args[1]} created successfully"
-
-    # route creation
-    when 'route'
-      if @routes.include?(args[1])
-        puts "The Route #{args[1]} already exists!"
-        return
-      end
-
-      if args.size < 4
-        puts CREATE_FORMAT_ERROR
-        return
-      end
-
-      unless @stations.include?(args[2])
-        puts "Error! Station #{args[2]} does not exist!"
-        return
-      end
-
-      unless @stations.include?(args[3])
-        puts "Error! Station #{args[3]} does not exist!"
-        return
-      end
-
-      @routes[args[1]] = Route.new(
-        @stations[args[2]], @stations[args[3]], args[1]
-      )
-      puts "The Route #{args[1]} from #{args[2]} "\
-        "to #{args[3]} created successfully"
-
-    # train creation
-    when 'train'
-      if @trains.include?(args[1])
-        puts "The Train #{args[1]} already exists!"
-        return
-      end
-
-      if args.size < 3
-        puts CREATE_FORMAT_ERROR
-        return
-      end
-
-      case args[2]
-      when 'cargo', 'car'
-        @trains[args[1]] = CargoTrain.new(args[1])
-        puts 'Cargo Train created successfully'
-      when 'passenger', 'p'
-        @trains[args[1]] = PassengerTrain.new(args[1])
-        puts 'Passenger Train created successfully'
-      else
-        puts CREATE_FORMAT_ERROR
-      end
+    when '--help' then puts CREATE_HELP
+    when 'station' then create_station(args)
+    when 'route' then create_route(args)
+    when 'train' then create_train(args)
     else
       puts CREATE_FORMAT_ERROR
     end
@@ -125,6 +105,104 @@ class TextUI
     true
   end
 
+  def add_station(args)
+    return unless correct_form?(args, %i[station route])
+
+    @routes[args[1]].add_station(@stations[args[0]])
+    puts "Station #{args[0]} was successfully added to the Route #{args[1]}"
+  end
+
+  def remove_station(args)
+    return unless correct_form?(args, %i[station route])
+
+    @routes[args[1]].remove_station(@stations[args[0]])
+    puts "Station #{args[0]} was successfully removed from the Route #{args[1]}"
+  end
+
+  def add_wagon(args)
+    return unless correct_form?(args, [:train])
+
+    case @trains[args[0]].class
+    when :PassengerTrain
+      @trains[args[0]].add_wagon(PassengerWagon.new)
+    when :CargoTrain
+      @trains[args[0]].add_wagon(CargoWagon.new)
+    end
+
+    puts "A wagon was successfully added to the Train #{args[0]}"
+  end
+
+  def remove_wagon(args)
+    return unless correct_form?(args, [:train])
+
+    @trains[args[0]].remove_last_wagon
+    puts "A wagon was successfully removed from the Train #{args[0]}"
+  end
+
+  def stations
+    puts 'At the moment these stations were created:'
+    @stations.each do |name, _st|
+      puts name
+    end
+  end
+
+  def assing(args)
+    return unless correct_form?(args, %i[route train])
+
+    @trains[args[1]].route = @routes[args[0]]
+    puts "The Route #{args[0]} was successfully assigned to the Train #{args[1]}"
+  end
+
+  def trains(args)
+    return unless correct_form?(args, [:station])
+
+    puts "At the moment these Trains are at the Station #{args[0]}:"
+    @stations[args[0]].trains.each do |tr|
+      puts tr.num
+    end
+  end
+
+  def move(args)
+    return unless correct_form?(args, %i[train any])
+
+    unless @trains[args[0]].route
+      puts "The train #{args[0]} has no Route assigned!"
+      return
+    end
+
+    case args[1]
+    when 'forward' then @trains[args[0]].move_forward
+    when 'back' then @trains[args[0]].move_backward
+    else
+      puts 'Error: wrong syntax. Trains could move only back and forward.'
+      puts HELP_REMINDER
+      return
+    end
+
+    puts "Train #{args[0]} was successfully moved #{args[1]} "\
+      "to the station #{@trains[args[0]].current_station}"
+  end
+
+  def run_command(command)
+    args = command [1..-1]
+    case command[0]
+    when 'q', 'quit' then running = false
+    when 'help', '?' then puts HELP
+    when 'create' then create_object(args)
+    when 'add-station' then add_station(args)
+    when 'remove-station' then remove_station(args)
+    when 'add-wagon' then add_wagon(args)
+    when 'remove-wagon' then remove_wagon(args)
+    when 'stations' then stations
+    when 'assign' then assign(args)
+    when 'trains' then trains(args)
+    when 'move' then move(args)
+    else
+      puts "There is no command '#{command[0]}'"
+      puts HELP_REMINDER
+    end
+  end
+
   def run
     running = true
     puts GREETINGS
@@ -133,82 +211,7 @@ class TextUI
     while running
       print '>'
       command = gets.split
-      case command[0]
-      when 'q'
-        running = false
-      when 'quit'
-        running = false
-      when 'help', '?'
-        puts HELP
-      when 'create'
-        create_object(command[1..-1])
-      when 'add-station'
-        next unless correct_form?(command[1..-1], %i[station route])
-
-        @routes[command[2]].add_station(@stations[command[1]])
-        puts "Station #{command[1]} was successfully added to the Route #{command[2]}"
-      when 'remove-station'
-        next unless correct_form?(command[1..-1], %i[station route])
-
-        @routes[command[2]].remove_station(@stations[command[1]])
-        puts "Station #{command[1]} was successfully removed from the Route #{command[2]}"
-      when 'add-wagon'
-        next unless correct_form?(command[1..-1], [:train])
-
-        case @trains[command[1]].class
-        when :PassengerTrain
-          @trains[command[1]].add_wagon(PassengerWagon.new)
-        when :CargoTrain
-          @trains[command[1]].add_wagon(CargoWagon.new)
-        end
-        puts "A wagon was successfully added to the Train #{command[1]}"
-      when 'remove-wagon'
-        next unless correct_form?(command[1..-1], [:train])
-
-        @trains[command[1]].remove_last_wagon
-        puts "A wagon was successfully removed from the Train #{command[1]}"
-      when 'stations'
-        puts 'At the moment these stations were created:'
-        @stations.each do |name, _st|
-          puts name
-        end
-      when 'assign'
-        next unless correct_form?(command[1..-1], %i[route train])
-
-        @trains[command[2]].route = @routes[command[1]]
-        puts "The Route #{command[1]} was successfully assigned to the Train #{command[2]}"
-      when 'trains'
-        next unless correct_form?(command[1..-1], [:station])
-
-        puts "At the moment these Trains are at the Station #{command[1]}:"
-        @stations[command[1]].trains.each do |tr|
-          puts tr.num
-        end
-      when 'move'
-        next unless correct_form?(command[1..-1], %i[train any])
-
-        unless @trains[command[1]].route
-          puts "The train #{command[1]} has no Route assigned!"
-          next
-        end
-
-        case command[2]
-        when 'forward'
-          @trains[command[1]].move_forward
-        when 'back'
-          @trains[command[1]].move_backward
-        else
-          puts 'Error: wrong syntax. Trains could move only back and forward.'
-          puts HELP_REMINDER
-          next
-        end
-
-        puts "Train #{command[1]} was successfully moved #{command[2]} "\
-          "to the station #{@trains[command[1]].current_station}"
-      else
-        puts "There is no command '#{command[0]}'"
-        puts HELP_REMINDER
-      end
+      run_command(command[0..-1]) unless command.empty?
     end
   end
 end
