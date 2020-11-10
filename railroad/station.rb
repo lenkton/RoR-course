b00ec58452c4bd85a11fe85966a3dd1711600fcb
lenkton <antonov_s@yahoo.com>
2020-11-10 20:18:21 +0300

    added 'all' class method to the Station class

--- railroad/station.rb
+++ railroad/station.rb
@@ -4,9 +4,14 @@
 class Station
   attr_reader :trains, :name
 
+  def self.all
+    @@stations
+  end
+
   def initialize(name)
     @name = name
     @trains = []
+    @@stations << self
   end
 
   def add_train(train)
@@ -21,4 +26,8 @@ class Station
   def send_train(train)
     @trains.delete train
   end
+  
+  private
+  
+  @@stations = []
 end
